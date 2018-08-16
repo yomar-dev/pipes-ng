6c4478313f22e40517600d941b873cce0541d886
Yomar Miranda <mirandayomar@gmail.com>
2018-08-15 22:11:43 -0500

    Pipe: JSON.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = 'Yomar';
-  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  PI = Math.PI;
-  num = 0.234;
-  salario = 1234.5;
+    nombre = 'Yomar';
+    arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    PI = Math.PI;
+    num = 0.234;
+    salario = 1234.5;
+    heroe = {
+        nombre: 'Logan',
+        clave: 'Wolverine',
+        edad: 700,
+        direccion: {
+            calle: 'Novena',
+            carrera: 20,
+            apartamento: '202'
+        }
+    }
 }
