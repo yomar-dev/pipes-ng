e444c63770f2988f0cd7857e4f893db6a145ba61
Yomar Miranda <mirandayomar@gmail.com>
2018-08-15 22:42:14 -0500

    Mostrar fechas en Español.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LOCALE_ID } from '@angular/core';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [
+        { provide: LOCALE_ID, useValue: 'es' }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
